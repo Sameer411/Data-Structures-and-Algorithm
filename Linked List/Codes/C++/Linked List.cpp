892a9b569f843332f77174014071ce0608597e7f
Sameer Rathod <sameerrathod1999@gmail.com>
2021-09-21 12:35:48 +0530

    Update Linked List.cpp

--- Linked List/Codes/C++/Linked List.cpp
+++ Linked List/Codes/C++/Linked List.cpp
@@ -1,127 +1,174 @@
-//  C++ Program for Linked List 
-/* Operation getting performed in code 
-
-1) Insertion of Data 
-2) Searching 
-3) Printing
-
-*/
-
 #include <iostream>
+ 
 using namespace std;
-
-struct node
-{
-        int num;
-        node *nextptr;
-} * stnode; //node defined
-
-void makeList(int n);
-void showList();
-void searchList(int item, int n);
-
-int main()
-{
-        int n, num, item;
-
-        cout << "Enter the number of nodes: ";
-        cin >> n;
-        makeList(n);
-        cout << "\nLinked list data: \n";
-        showList();
-        cout << "\nEnter element you want to search: ";
-        cin >> item;
-        searchList(item, n);
-
-        return 0;
+ 
+//Declare Node 
+struct Node{
+    int num;
+    Node *next;
+};
+ 
+//Declare starting (Head) node
+struct Node *head=NULL;
+ 
+//Insert node at start
+void insertNode(int n){
+    struct Node *newNode=new Node;
+    newNode->num=n;
+    newNode->next=head;
+    head=newNode;
 }
-void makeList(int n) //function to create linked list.
-{
-        struct node *frntNode, *tmp;
-        int num, i;
-
-        stnode = (struct node *)malloc(sizeof(struct node));
-        if (stnode == NULL)
-        {
-                cout << " Memory can not be allocated";
-        }
-        else
-        {
-
-                cout << "Enter the data for node 1: ";
-                cin >> num;
-                stnode->num = num;
-                stnode->nextptr = NULL; //Links the address field to NULL
-                tmp = stnode;
-
-                for (i = 2; i <= n; i++)
-                {
-                        frntNode = (struct node *)malloc(sizeof(struct node));
-
-                        if (frntNode == NULL) //If frntnode is null no memory cannot be allotted
-                        {
-                                cout << "Memory can not be allocated";
-                                break;
-                        }
-                        else
-                        {
-                                cout << "Enter the data for node " << i << ": "; // Entering data in nodes.
-                                cin >> num;
-                                frntNode->num = num;
-                                frntNode->nextptr = NULL;
-                                tmp->nextptr = frntNode;
-                                tmp = tmp->nextptr;
-                        }
-                }
-        }
+ 
+//Traverse/ display all nodes (print items)
+void display(){
+    if(head==NULL){
+        cout<<"List is empty!"<<endl;
+        return;
+    }
+    struct Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->num<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
 }
-
-void showList() //function to print linked list
-{
-        struct node *tmp;
-        if (stnode == NULL)
-        {
-                cout << "No data found in the list";
-        }
-        else
-        {
-                tmp = stnode;
-                cout << "Linked List: ";
-                while (tmp != NULL)
-                {
-                        cout << "\t" < num;
-                        tmp = tmp->nextptr;
-                }
-        }
+ 
+//delete node from start
+void deleteItem(){
+    if(head==NULL){
+        cout<<"List is empty!"<<endl;
+        return;
+    }
+    cout<<head->num<<" is removed."<<endl;
+    head=head->next;
 }
-void searchList(int item, int n) //function to search element in the linked list
-{
-        struct node *tmp;
-        int i = 0, flag;
-        tmp = stnode;
-        if (stnode == NULL)
-        {
-                cout << "\nEmpty List\n";
-        }
-        else
-        {
-                while (tmp != NULL)
-                {
-                        if (tmp->num == item) //If element is present in the list
-                        {
-                                cout << "Item found at location: " << (i + 1);
-                                flag = 0;
-                        }
-                        else
-                        {
-                                flag++;
-                        }
-                        i++;
-                        tmp = tmp->nextptr;
-                }
-                if (flag == n) //If element is not present in the list
-                {
-                        cout << "Item not found\n";
-                }
-        }
+int main(){
+     
+    display();
+    insertNode(10);
+    insertNode(20);
+    insertNode(30);
+    insertNode(40);
+    insertNode(50);
+    display();
+    deleteItem(); deleteItem(); deleteItem(); deleteItem(); deleteItem();
+    deleteItem();
+    display();
+    return 0;
+}
+#include <iostream>
+ 
+using namespace std;
+ 
+//Declare Node 
+struct Node{
+    int num;
+    Node *next;
+};
+ 
+//Declare starting (Head) node
+struct Node *head=NULL;
+ 
+//Insert node at start
+void insertNode(int n){
+    struct Node *newNode=new Node;
+    newNode->num=n;
+    newNode->next=head;
+    head=newNode;
+}
+ 
+//Traverse/ display all nodes (print items)
+void display(){
+    if(head==NULL){
+        cout<<"List is empty!"<<endl;
+        return;
+    }
+    struct Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->num<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+ 
+//delete node from start
+void deleteItem(){
+    if(head==NULL){
+        cout<<"List is empty!"<<endl;
+        return;
+    }
+    cout<<head->num<<" is removed."<<endl;
+    head=head->next;
+}
+int main(){
+     
+    display();
+    insertNode(10);
+    insertNode(20);
+    insertNode(30);
+    insertNode(40);
+    insertNode(50);
+    display();
+    deleteItem(); deleteItem(); deleteItem(); deleteItem(); deleteItem();
+    deleteItem();
+    display();
+    return 0;
+}
+#include <iostream>
+ 
+using namespace std;
+ 
+//Declare Node 
+struct Node{
+    int num;
+    Node *next;
+};
+ 
+//Declare starting (Head) node
+struct Node *head=NULL;
+ 
+//Insert node at start
+void insertNode(int n){
+    struct Node *newNode=new Node;
+    newNode->num=n;
+    newNode->next=head;
+    head=newNode;
+}
+ 
+//Traverse/ display all nodes (print items)
+void display(){
+    if(head==NULL){
+        cout<<"List is empty!"<<endl;
+        return;
+    }
+    struct Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->num<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+ 
+//delete node from start
+void deleteItem(){
+    if(head==NULL){
+        cout<<"List is empty!"<<endl;
+        return;
+    }
+    cout<<head->num<<" is removed."<<endl;
+    head=head->next;
+}
+int main(){
+     
+    display();
+    insertNode(10);
+    insertNode(20);
+    insertNode(30);
+    insertNode(40);
+    insertNode(50);
+    display();
+    deleteItem(); deleteItem(); deleteItem(); deleteItem(); deleteItem();
+    deleteItem();
+    display();
+    return 0;
 }
